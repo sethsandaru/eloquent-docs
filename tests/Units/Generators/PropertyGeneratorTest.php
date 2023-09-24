45eba4f607830beaad0f648b6f6f6b674aeb2c29
Seth Phat <phattranminh96@gmail.com>
2023-09-24 14:34:55 +0700

    PgSQL Numeric will be rendered as Float (#17)

--- tests/Units/Generators/PropertyGeneratorTest.php
+++ tests/Units/Generators/PropertyGeneratorTest.php
@@ -55,4 +55,25 @@ class PropertyGeneratorTest extends TestCase
 
         $this->assertStringContainsString('@property bool|null $hello_bool', $generatedText);
     }
+
+    public function testNumericPostgresRenderFloat()
+    {
+        if (env('DB_CONNECTION') !== 'pgsql') {
+            $this->markTestSkipped('only available for pgsql');
+            return;
+        }
+
+        DB::unprepared("
+            CREATE TABLE test_numeric(
+                number numeric NOT NULL
+            )
+        ");
+
+        $columnGenerator = app(ColumnsGenerator::class);
+        $generatedText = $columnGenerator->generate(new class extends Model {
+            protected $table = 'test_numeric';
+        });
+
+        $this->assertStringContainsString('@property float $number', $generatedText);
+    }
 }
